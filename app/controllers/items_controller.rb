98ba43fc2d92aabf4002db94023bcbc1abe83aec
Antek <antoni.pstras@gmail.com>
2015-04-20 01:20:29 +0200

    basic warehouse stuff crud with paperclip

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -69,6 +69,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :quantity, :category)
+      params.require(:item).permit(:name, :quantity, :category, :photo)
     end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,8 @@ class Item
   field :quantity, type: Integer
   field :category, type: String
 
-  has_mongoid_attached_file :photo
+  has_mongoid_attached_file :photo,
+                            :styles => { :small => '100x100#', :medium => '250x250#', :large => '500x500#' }
+
+  validates_attachment_content_type :photo, :content_type => ['image/jpg', 'image/jpeg', 'image/png']
 end
